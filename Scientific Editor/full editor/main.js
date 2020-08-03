8017ff45b165ba33bdd3997e93efe4566653768e
Shantanu45 <30995267+Shantanu45@users.noreply.github.com>
2020-08-03 14:00:59 +0530

    final editor implementation

--- Scientific Editor/full editor/main.js
+++ Scientific Editor/full editor/main.js
@@ -12,11 +12,21 @@ var quill = new Quill('#editor-container', {
     // Populate hidden form on submit
     var content = document.querySelector('input[name=content]');
     content.value = JSON.stringify(quill.getContents());
-    
+    var DeltaContent = quill.getContents();
     console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+    // quill.setContents([
+    //   { insert: 'Hello ' },
+    //   { insert: 'World!', attributes: { bold: true } },
+    //   { insert: '\n' }
+    // ]);
+
+    // setTimeout(function(){ quill.setContents(quill.setContents(DeltaContent)); }, 3000)
     
+    // console.log("Submitted", $(form).serialize(), $(form).serializeArray(), 
+    // quill.setContents(quill.getContents()));
     // No back end to actually submit to!
     alert('Open the console to see the submit data!')
+
     return false;
   };
   
